548e00519f25777c91420bbd2221c8729bbd407c
Leestan360 <stanleymayore2@gmail.com>
2023-03-21 22:53:31 +0300

    Add boiler plate code

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="font-[Nunito]">
+      <p className="text-red-500">fghjioiuyt</p>
+    </div>
+  );
+}
+
+export default App;
