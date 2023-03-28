a8ab0c17609c444b78d1116ebfc2cf0dff94fef4
Leestan360 <stanleymayore2@gmail.com>
2023-03-28 20:29:29 +0300

    Restructure project

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+export function App() {
+
+  // const [toggle, setToggle] = React.useState(false)
+
+  
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default App;
---
+++ src/pages/home/HomePage.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Home from "../../components/home";
+
+function HomePage() {
+  return (
+    <div>
+      <Home />
+    </div>
+  )
+}
+
+export default HomePage;
