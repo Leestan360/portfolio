a62b6ed850e546a84b892e700d5ffe3eed23e1e9
realonbebeto <projobs254@gmail.com>
2023-05-27 15:29:24 +0300

    styling opensource card

--- src/components/opensourceproject/gitinfo.tsx
+++ src/components/opensourceproject/gitinfo.tsx
@@ -1,13 +1,31 @@
 import React from 'react'
+import { IconType } from 'react-icons'
 
 type Props = {
-  element: any
+  element: any;
+  style?: string;
+  Icon?: IconType;
 }
 
-const Element = ({ element }: Props): JSX.Element | any => {
-  return element ? (
-    <span className='text-sm font-thin m-1 py-1 px-3 text-[#1DA1F2]'>{element}</span>
-  ) : null
+const Element = ({ element, style, Icon }: Props): JSX.Element | any => {
+return element ? (
+    <div className="flex">
+      { Icon && (
+      <div className="text-sm">
+        <Icon/>
+      </div>
+      )}
+      <div>
+        <p className={`text-sm font-thin m-1 py-1 px-3 text-[#1DA1F2] ${style}`}>{element}</p>
+      </div>
+
+    </div>
+    ) : null
 }
+//   return element ? (
+//     <span className={`text-sm font-thin m-1 py-1 px-3 text-[#1DA1F2] ${style}`}>{element}</span>
+//   ) : null
+// }
 
 export default Element
+
