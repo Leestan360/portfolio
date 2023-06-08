c8b552299978216abba92c92d9bb57f737f6522c
realonbebeto <projobs254@gmail.com>
2023-06-08 08:10:22 +0300

    git profile and read

--- gitprofile.ts
+++ gitprofile.ts
@@ -1,39 +1,44 @@
-import * as fs from 'fs';
-import process from 'process';
-import { graphql } from "@octokit/graphql";
-
+const fs = require('fs');
+const { process } = require('process');
+const { graphql, GraphqlResponseError } = require("@octokit/graphql");
 
 const ERR = {
   noUserName:
     "Github Username was found to be undefined. Please set all relevant environment variables.",
   requestFailed:
     "The request to GitHub didn't succeed. Check if GitHub token in your .env file is correct.",
+  graphqlError: "Graphql Response Error"
 };
 
+//process.env.
 if (process.env.GITHUB_USERNAME === undefined) {
     throw new Error(ERR.noUserName);
   };
 
 const graphqlWithAuth = graphql.defaults({
   headers: {
-    authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+    authorization: `token ${process.env.GITHUB_TOKEN}`   //
   },
 });
 
 // Define your GraphQL query and execute the request
-async function fetchProfileRequest(query: string) {
+async function fetchProfileRequest(query: any) {
   try {
     const response = await graphqlWithAuth(query);
     return response;
   } catch (error: any) {
     // Handle the error, such as logging or displaying a friendly message
-    console.error(`${ERR.requestFailed}: ${error.message}`);
+    if (error instanceof GraphqlResponseError) {
+    console.error(`${ERR.graphqlError}: ${error.message}`);
     throw error;
+    } else {
+      console.error(`${ERR.requestFailed}: ${error.message}`);
+      throw error
+    }
   }
 }
 
-var data = JSON.stringify({
-    query: `
+var query = `
 {
   user(login:"${process.env.GITHUB_USERNAME}") { 
     name
@@ -65,16 +70,14 @@ var data = JSON.stringify({
       }
     }
 }
-`
-  });
+`;
 
 console.log(`Fetching profile data for ${process.env.GITHUB_USERNAME}`);
-  fetchProfileRequest(data).then((response: any)=> {
-    if (response.statusCode !== 200){
-        throw new Error(ERR.requestFailed);
-    }
-    fs.writeFile("./public/profile.json", JSON.stringify(response.data), function(err){
-        if (err) return console.log(err);
-        console.log("saved file to public/profile.json");
-    });
-  })
+
+fetchProfileRequest(query).then((response: any)=> {
+  var response_data = JSON.stringify(response)
+  fs.writeFile("./public/profile.json", response_data, 'utf8', (err: any) => {
+      if (err) return console.log(err);
+      console.log("saved file to public/profile.json");
+  });
+})
