848f62fb4fab100c8c1f82dfa4251070ea999711
realonbebeto <projobs254@gmail.com>
2023-06-11 17:09:15 +0300

    blog conditional render

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -7,7 +7,7 @@ const Blog = () => {
     const blogSectionStyle = "flex flex-col my-24"
     const blogHeaderStyle = "text-2xl font-light text-[#1DA1F2]"
     const blogCardContainerStyle = "flex flex-wrap justify-between pt-5 gap-y-9"
-  return (
+  return (blogInfo.display) ? (
     <section className={blogSectionStyle} id="musings">
         <div className="w-auto">
           <h1 className={blogHeaderStyle}>{blogInfo.title}</h1>
@@ -18,7 +18,7 @@ const Blog = () => {
           ))}
         </div>
     </section>
-  )
+  ) : null
 }
 
 export default Blog
